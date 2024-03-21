163442a6cf3b59049f95ecc35ede3af7eb50a809
Алексей Бельченков <u608110@gmail.com>
2024-03-21 15:19:08 +0400

    Homepage Components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import '@/assets/styles/globals.css';
 
 export const metadata = {
@@ -19,6 +20,7 @@ const MainLayout = ({ children }) => {
             <body>
                 <Navbar />
                 <main>{children}</main>
+                <Footer />
             </body>
         </html>
     );
